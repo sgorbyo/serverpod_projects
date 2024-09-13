39143f311b166ffd82fcafe725c598e0076cdb91
sgorbyo <sgorbyo@gmail.com>
2024-09-13 23:31:48 +0200

    first commit

---
+++ mypod/mypod_flutter/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:mypod_client/mypod_client.dart';
+import 'package:flutter/material.dart';
+import 'package:mypod_flutter/src/widgets/account_page.dart';
+import 'package:mypod_flutter/src/widgets/sign_in_page.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'src/serverpod_client.dart';
+
+// Sets up a singleton client object that can be used to talk to the server from
+// anywhere in our app. The client is generated from your server code.
+// The client is set up to connect to a Serverpod running on a local server on
+// the default port. You will need to modify this to connect to staging or
+// production servers.
+var client = Client('http://$localhost:8080/')
+  ..connectivityMonitor = FlutterConnectivityMonitor();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeServerpodClient();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Serverpod Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Serverpod Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body:
+          sessionManager.isSignedIn ? const AccountPage() : const SignInPage(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Make sure that we rebuild the page if signed in status changes.
+    sessionManager.addListener(() {
+      setState(() {});
+    });
+  }
+}
